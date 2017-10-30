c3dad11c6f8207e171f1712390e79cd5fec236cb
Unknown <realague@gmail.com>
2017-10-30 20:35:15 +0100

    wip

---
+++ Vation/Assets/scripts/Bounce.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+	private float lerpTime;
+	private float currentLerpTime;
+	private float perc = 1;
+
+	public Vector3 startPos;
+	public Vector3 endPos;
+
+	private void Start()
+	{
+
+	}
+	
+	private void Update()
+	{
+		if (Input.GetButtonDown("z") || Input.GetButtonDown("q") || Input.GetButtonDown("s") || Input.GetButtonDown("d"))
+		{
+			if (perc == 1)
+			{
+				
+			}
+		}
+	}
+
+}
