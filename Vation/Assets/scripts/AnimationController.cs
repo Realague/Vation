aab2e20a1cb7dfd8776f66e8190fde2028f049cc
Realague <realague@gmail.com>
2017-11-06 23:26:57 +0100

    added jump

---
+++ Vation/Assets/scripts/AnimationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+	Animator animator;
+	public GameObject player;
+
+	void Start () {
+		animator = gameObject.GetComponent<Animator> ();
+	}
+	
+	void Update () {
+		Bounce bounceScript = player.GetComponent<Bounce> ();
+		if (bounceScript.justJump == true) {
+			animator.SetBool("Jump", true);
+		} else {
+			animator.SetBool("Jump", false);
+		}
+	}
+}
--- Vation/Assets/scripts/Bounce.cs
+++ Vation/Assets/scripts/Bounce.cs
@@ -2,27 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bounce : MonoBehaviour
-{
+public class Bounce : MonoBehaviour {
 	private float lerpTime;
 	private float currentLerpTime;
 	private float perc = 1;
 
-	public Vector3 startPos;
-	public Vector3 endPos;
+	private Vector3 startPos;
+	private Vector3 endPos;
 
-	private void Start()
-	{
+	private bool firstInput;
+	public bool justJump;
+
+	void Start () {
 
 	}
 	
-	private void Update()
-	{
-		if (Input.GetButtonDown("z") || Input.GetButtonDown("q") || Input.GetButtonDown("s") || Input.GetButtonDown("d"))
+	void Update () {
+		if (Input.GetButtonDown("up") || Input.GetButtonDown("right") || Input.GetButtonDown("down") || Input.GetButtonDown("left"))
 		{
 			if (perc == 1)
 			{
-				
+				lerpTime = 1;
+				currentLerpTime = 0;
+				firstInput = true;
+				justJump = true;
+			}
+		}
+		startPos = gameObject.transform.position;
+		if (Input.GetButtonDown("right") && gameObject.transform.position == endPos) {
+			endPos = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+		} else if (Input.GetButtonDown("up") && gameObject.transform.position == endPos) {
+			endPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+		} else if (Input.GetButtonDown("left") && gameObject.transform.position == endPos) {
+			endPos = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+		} else if (Input.GetButtonDown("down") && gameObject.transform.position == endPos) {
+			endPos = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+		}
+		if (firstInput == true) {
+			currentLerpTime += Time.deltaTime * 5;
+			perc = currentLerpTime / lerpTime;
+			gameObject.transform.position = Vector3.Lerp(startPos, endPos, perc);
+			if (perc > 0.9) {
+				perc = 1;
+			}
+			if (Mathf.Round(perc) == 1) {
+				justJump = false;
 			}
 		}
 	}
